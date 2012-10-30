34f684e844911122c61bfdfaefbbf84fa571195f
ryanfiorini <ryanwfiorini@gmail.com>
2012-10-30 14:31:02 -0400

    init repo

---
+++ AppMobiWP8HelloWorldTemplate/_MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using AppMobiWP8HelloWorldTemplate.Resources;
+using appMobiWebViewWP8;
+
+namespace AppMobiWP8HelloWorldTemplate
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public appMobiWebView amWebView;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            amWebView = new appMobiWebView(webView, phoneApplicationPage, "html/index.html");
+        }
+    }
+}
